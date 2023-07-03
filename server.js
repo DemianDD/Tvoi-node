dced2248d7b0e802d0021c47de6b41cd5749fdbc
DemianDD <thvarichdemyan@icloud.com>
2023-07-03 22:59:27 +0300

    add backen logic and update frntend

--- server.js
+++ server.js
@@ -1,11 +1,55 @@
 const express = require('express');
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
 
 const path = require('path');
-
+const cors = require('cors')
 const app = express();
 
 app.use(express.static(path.join(__dirname + '/public')));
 
-const PORT = process.env.PORT || 5000;
+app.use(cors());
 
-app.listen(PORT);
+app.use(bodyParser.json());
+
+app.post("/order", (req, res) => {
+  const order = req.body;
+
+  sendOrderEmail(order)
+    .then(() => {
+      res.sendStatus(200);
+    })
+    .catch((error) => {
+      console.error("Error sending order email:", error);
+      res.sendStatus(500);
+    });
+});
+
+async function sendOrderEmail(order) {
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "alexxholly11@gmail.com",
+        pass: "mctzcmpnwybnvcqr",
+      },
+    });
+
+    const mailOptions = {
+      from: "alexxholly11@gmail.com",
+      to: "tvoishopua@gmail.com",
+      subject: "New Order",
+      text: JSON.stringify(order, null, 2),
+    };
+
+    await transporter.sendMail(mailOptions);
+  } catch (error) {
+    console.error("Error sending order email:", error);
+    throw error;
+  }
+}
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log('Server listening on port', PORT)
+});
