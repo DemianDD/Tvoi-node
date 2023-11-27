d9a159f76d5be1ba066e85ede1ad06fb4830eabc
AlexB <alex.bodnar315@gmail.com>
2023-11-27 12:59:02 +0200

    update sequelize config file

---
+++ sequelize/config/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  development: {
+    url: 'postgres://root:0T4VDHyVkrjW9dXCltrBTfezfvjmybmz@dpg-ckoduns1tcps73f7snm0-a.frankfurt-postgres.render.com/tvoidb',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+  },
+  production: {
+    url: process.env.DB_URL,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  }
+};
